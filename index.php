1bf7de65a25a0f94b248c5b1b456910a7b01c51c
TarcisioAlvesFernandes <tarcisiorevolta@gmail.com>
2020-06-06 14:34:43 -0300

    Fiz Select de uma lista, e de login e senha

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
 
 require_once("config.php");
-/*
+
 $sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
-*/
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//echo json_encode($usuarios);
 
-$jose = new Usuario();
 
-$jose->loadById(1);
 
-echo $jose;
+//Carrega um usuario
+//$jose = new Usuario();
+//$jose->loadById(1);
+//echo $jose;
+
+
+//Carrega uma Lista de Usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de Usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jose","1234567890");
+
+echo $usuario;
+
 
 ?>
